197d6b2b5e4e466955d3a1c911b9c8461b0be303
Kai <wangkai.kaizen@gmail.com>
2019-09-29 15:51:38 +0800

    ADD: init worker

--- script/init-master.bash
+++ script/init-master.bash
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+apt-get update
+apt-get install -y docker.io kubeadm
+
 kubeadm config images pull --config ../config/kubeadm.yaml
 kubeadm init --config ../config/kubeadm.yaml
--- script/init.bash
+++ script/init.bash
@@ -4,6 +4,3 @@ curl https://mirrors.aliyun.com/kubernetes/apt/doc/apt-key.gpg | apt-key add -
 cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
 deb https://mirrors.aliyun.com/kubernetes/apt/ kubernetes-xenial main
 EOF
-
-apt-get update
-apt-get install -y docker.io kubeadm
